04316f260710473fa50f8c4b1690b72dca5671a5
Rian Negreiros <riannegreiros@gmail.com>
2021-12-30 19:40:18 -0300

    deposit

---
+++ pset7/despoit.php
@@ -0,0 +1,30 @@
+<?php
+
+    // configuration
+    require("../includes/config.php"); 
+
+    // if form is submitted
+    if ($_SERVER["REQUEST_METHOD"] == "POST") 
+    {
+        // if deposit amount is invalid (not a whole positive integer)
+        if (preg_match("/^\d+$/", $_POST["deposit"]) == false)
+        {
+            // apologize
+            apologize("You must enter a whole, positive integer.");
+        }
+        
+        // update user's cash
+        query("UPDATE users SET cash = cash + ? WHERE id = ?", $_POST["deposit"], $_SESSION["id"]);
+        
+        // redirect to portfolio 
+        redirect("/");
+    }
+    
+    // if form hasn't been submitted
+    else
+    {
+        // render portfolio (pass in new portfolio table and cash)
+        render("deposit_form.php", ["title" => "Deposit Form"]);
+    }
+
+?>
