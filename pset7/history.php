1a78569e8f181373027abcdc83112c3e09d3f1e9
Rian Negreiros <riannegreiros@gmail.com>
2021-12-30 19:41:30 -0300

    history

---
+++ pset7/history.php
@@ -0,0 +1,15 @@
+<?php
+
+    // configuration
+    require("../includes/config.php");  
+    
+    // query cash for template
+    $cash =	query("SELECT cash FROM users WHERE id = ?", $_SESSION["id"]);	
+    
+	// create new array to store all info for history table
+    $table = query("SELECT * FROM history WHERE id = ?", $_SESSION["id"]);
+    
+    // render sell form
+    render("history_form.php", ["title" => "History", "cash" => $cash, "table" => $table]);
+   
+?>
